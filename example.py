3064a639a44d36dd7c009de2867740718fc1f0a1
AntN21 <antoine.olivier@eleves.enpc.fr>
2024-07-16 16:18:25 +0200

    files

---
+++ example.py
@@ -0,0 +1,18 @@
+
+SVM = 0
+MLP = 1
+CNN = 2
+if __name__ == "__main__":
+    print("Hello, World!")
+
+class SupportVectorMachine(truc):
+    pass
+
+class MultiPerceptron(truc):
+    pass
+
+class ConvotionalNeuralNetwork(truc):
+    pass
+
+
+f(blabla,MLP)
